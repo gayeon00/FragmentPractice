f9a8861124ff5248f33f1328d24a4c956281168e
gayeon00 <chlrkdus4819@gmail.com>
2022-04-30 21:36:00 +1000

    feat: 프라그먼트 액티비티 데이터 전송
    - 프라그먼트에서 액티비티 함수 사용
    - 프라그먼트에서의 조작으로 액티비티에 영향주기

--- app/src/main/java/com/example/fragmentpractice/FirstFragment.kt
+++ app/src/main/java/com/example/fragmentpractice/FirstFragment.kt
@@ -1,9 +1,11 @@
 package com.example.fragmentpractice
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class FirstFragment : Fragment() {
@@ -17,6 +19,21 @@ class FirstFragment : Fragment() {
         //container: 프라그먼트에서 사용될 XML의 부모뷰
 
         //container의 자식 뷰로 R.layout.fragment_first레이아웃을 붙여줌
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
+
+        view.findViewById<TextView>(R.id.call_activity).setOnClickListener {
+            //여러 액티비티가 이 프라그먼트를 사용할 수 있어서 어떤 액티비티 인지 정확히 캐스팅 해주기
+            (activity as FragmentActivity).printTestLog()
+        }
+
+        return view
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //데이터를 찾아오는 작업을 할 때는 프라그먼트도 onCreate에서 하도록!
+
+        val data = arguments?.getString("key")
+        Log.d("testt", "data is "+data)
     }
 }
--- app/src/main/java/com/example/fragmentpractice/FragmentActivity.kt
+++ app/src/main/java/com/example/fragmentpractice/FragmentActivity.kt
@@ -2,6 +2,7 @@ package com.example.fragmentpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 
 class FragmentActivity : AppCompatActivity() {
@@ -15,6 +16,11 @@ class FragmentActivity : AppCompatActivity() {
         //Transaction: 작업의 단위
         (findViewById<TextView>(R.id.add)).setOnClickListener {
             val transaction = fragmentManager.beginTransaction() //트랜잭션의 시작
+            //프라그먼트에 데이터를 전달하는 방법
+            val bundle = Bundle()
+            bundle.putString("key", "hello")
+            firstFragment.arguments = bundle
+
             transaction.replace(R.id.root, firstFragment) //이 액티비티에서 root로 해 놓은데다가 프라그먼트를 붙임
             transaction.commit() //끝 : 끝을 내줘야 한 트랜잭션이 정의되는거라 그제서야  실행됨!
         }
@@ -27,5 +33,9 @@ class FragmentActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    fun printTestLog(){
+        Log.d("testt", "print test log")
     }
 }
